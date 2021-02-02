9b7e463cbf34ea61867668bb529921da7403c118
elenabarashkova <lena.barashkova23@gmail.com>
2021-02-02 17:11:43 +0300

    feat: function check for string parameter

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    // 1 - string check
+    if(str[0] === ')' || str[0] === '}' || str[0] === ']') {
+        return false;   
+    }
+
+    const roundO = '(';
+    const roundC = ')';
+    const squareO = '[';
+    const squareC = ']';
+    const figuredO = '{';
+    const figuredC = '}';
+    let toBeClosed = [];
+
+    for (let i = 0; i < str.length; i++) {
+        if(str[i] === roundO || str[i] === squareO || str[i] === figuredO) {
+            toBeClosed.push(str[i]);
+        } 
+        else {
+            let lastNotClosed = toBeClosed.pop();
+            if(lastNotClosed === roundO && str[i] != roundC || lastNotClosed === squareO && str[i] != squareC || lastNotClosed === figuredO && str[i] != figuredC) {
+            return false;
+            }
+        }
+    }
+    if(toBeClosed.length > 0) {
+        return false;
+    } else {
+        return true;
+    }   
 }
