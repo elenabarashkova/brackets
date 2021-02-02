887ffdf520f5f1b52fcead2199d7f2e21906dc2a
elenabarashkova <lena.barashkova23@gmail.com>
2021-02-02 23:53:49 +0300

    fix: fixed function for special cases

--- src/index.js
+++ src/index.js
@@ -1,35 +1,44 @@
 module.exports = function check(str, bracketsConfig) {
-    const openedBrackets = [];
-    const closedBrackets =[]; 
-     for (let i = 0; i < bracketsConfig.length; i++) {
-      openedBrackets.push(bracketsConfig[i][0]);
-      closedBrackets.push(bracketsConfig[i][1]);
-    } 
-    
-    for (let i = 0; i < bracketsConfig.length; i++) {
-        if(str[0] === bracketsConfig[i][1]) {
-      return false;
+     
+  const openedBrackets = [];
+  const closedBrackets =[]; 
+  // циклом выделить все открытые и все закрытые скобки в отдельныe массивы из параметра bracketsConfig
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    openedBrackets.push(bracketsConfig[i][0]);
+    closedBrackets.push(bracketsConfig[i][1]);
+  } 
+  
+  const toBeClosed = []; //для стека открытых скобок
+  
+  for (let i = 0; i < str.length; i++) {
+  //если скобка закрытая (сравнивается i-тый элемент str со всеми элементами openedBrackets и если совпадения нет, то выдается -1. в противном случае выдастся индекс совпадающего элемента)
+    if(closedBrackets.indexOf(str[i]) !== -1 && toBeClosed.length) {
+			//Если скобка закрытая, то нужно проверить соответствует ли она последней незакрытой из toBeClosed. 
+      //Если да, то сделать pop из toBeClosed
+      //Если нет, то начать новую итерацию через continue
+      let lastNotClosed = toBeClosed[toBeClosed.length - 1]; //Последний элемент toBeClosed
+      // Индекс последнего lastNotClosed в массиве bracketsConfig. 
+      // Такой же индекс должен быть у текущего элемента если он соответсвует lastNotClosed
+      const openedBracketIndex = openedBrackets.indexOf(lastNotClosed);
+      if(closedBrackets[openedBracketIndex] === str[i]) {
+        toBeClosed.pop();
+        continue;
       }
     }
-    
-    const toBeClosed = [];
-     
-    for (let i = 0; i < str.length; i++) {
-      if(openedBrackets.indexOf(str[i]) !== -1) {
-        toBeClosed.push(str[i]);
-      } 
-      else {
-        let lastNotClosed = toBeClosed.pop();
-        const openedBracketIndex = openedBrackets.indexOf(lastNotClosed);
-        if(closedBrackets[openedBracketIndex] !== str[i]) {
-          return false;
-        }
-      }   
+    // Если скобка либо не соответствует lastNotClosed, либо открытая при проверке
+    // Если скобка открыая, то push в toBeClosed 
+    if(openedBrackets.indexOf(str[i]) !== -1) {
+    	toBeClosed.push(str[i]);
     }
-    if(toBeClosed.length > 0) {
-      return false;
-    }
+    // Если нет, то вернуть false
     else {
-    return true;
-    }  
+    return false;
+    }
+  }
+  if(toBeClosed.length > 0) {
+    return false;
+  }
+  else {
+  return true;
+  } 
 }
